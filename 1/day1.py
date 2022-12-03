b4d43f0c1c9027340e78d2db4720cefc16a228a2
hana <17885726+hgonzal@users.noreply.github.com>
2022-12-03 13:33:27 +0100

    Simplify day 1

--- 1/day1.py
+++ 1/day1.py
@@ -3,24 +3,19 @@
 elves = [0]
 with open("input", "r") as infile:
     i = 0
-    max = 0
     for line in infile:
         if not line==("\n"):
             #print(str(i) + " " + line)
             elves[i] += int(line)
         else:
-            if elves[i] > max: 
-                max = elves[i]
             elves.append(0)
             i = i+1
 
-    #print(max)
+#Part 1
+print(max(elves))
 
-    elves.sort() 
-    sums = 0 
-    for elf in elves[-3:]: 
-        sums += elf 
-    print(sums) 
+#Part 2
+elves.sort() 
+print(sum(elves[-3:])) 
 
-    #print(elves[-3:])
-    #print(str(len(elves)))
+#print(str(len(elves)))
