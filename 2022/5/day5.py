0f70a8e16bc5abfec6d51cd60b4953e89db06f70
hana <17885726+hgonzal@users.noreply.github.com>
2022-12-06 18:31:07 +0100

    Change day 5 solution to use 'proper' stacks (add/insert from end)

--- 2022/5/day5.py
+++ 2022/5/day5.py
@@ -11,7 +11,9 @@ with open("input", "r") as infile:
         if line =="\n" or line[1] =="1" :
             continue 
         elif line.startswith("move"):
-            if stacks2 is None: 
+            if stacks2 is None:
+                for s in stacks.values(): 
+                    s.reverse() 
                 stacks2 = copy.deepcopy(stacks)
 
             line = line.split(" ")
@@ -21,29 +23,29 @@ with open("input", "r") as infile:
 
             #PART 1
             for i in range(qty): 
-                crate = stacks[frm].pop(0)
-                stacks[to].insert(0,crate)
+                crate = stacks[frm].pop()
+                stacks[to].append(crate)
 
             #PART 2
-            for i in range(qty-1,-1,-1):
-                crate = stacks2[frm].pop(i)
-                stacks2[to].insert(0,crate)
+            for i in range(qty,0,-1):
+                #print(i, stacks2[frm])
+                crate = stacks2[frm].pop(i*-1)
+                stacks2[to].append(crate)
 
 
         else:
             for i in range(1,len(line),4): 
                 if line[i] != " ": 
-                    print(i, line[i])
                     key = (i+3)/4
                     stacks[key].append(line[i])
 
     print("\n", stacks)
     for stack in stacks.values(): 
         if len(stack) > 0: 
-            print(stack[0], end="")
+            print(stack[-1], end="")
        
     print("\n", stacks2)
     for stack in stacks2.values(): 
         if len(stack) > 0: 
-            print(stack[0], end="")
+            print(stack[-1], end="")
 
